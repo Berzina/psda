aad28416fc36d0ece9255a3a0de1fe0de1b25180
Ekaterina Khotko <ekhotko@plgn.ru>
2016-12-09 13:42:31 +0500

    medai root statis

--- smarthome/urls.py
+++ smarthome/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include, patterns
+from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from . import settings
 
@@ -26,4 +27,4 @@ urlpatterns = [
 urlpatterns += patterns('',
         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     )
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
